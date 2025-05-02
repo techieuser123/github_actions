4ae8c5fae414e66d930f6881d945a1fc8adee5f0
techieuser <projectmail2223@gmail.com>
2025-05-02 15:47:00 +0530

    test

--- bookhive/settings.py
+++ bookhive/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://github-actions-cie5.onrender.com','*']
+ALLOWED_HOSTS = ['https://github-actions-cie5.onrender.com','http://my-test-env.eba-sb6nhzsh.ap-northeast-3.elasticbeanstalk.com/','*']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://github-actions-cie5.onrender.com',
